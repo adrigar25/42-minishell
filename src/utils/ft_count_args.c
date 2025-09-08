63a7f903c8487eb694dd7c29acbfcb2d4deca544
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2025-09-08 15:58:31 +0200

    Subida

--- src/utils/ft_count_args.c
+++ src/utils/ft_count_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_count_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: adriescr <adriescr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 14:20:19 by adriescr          #+#    #+#             */
-/*   Updated: 2025/09/05 14:25:17 by agarcia          ###   ########.fr       */
+/*   Updated: 2025/09/08 15:55:56 by adriescr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int	ft_count_args(const char *cmd)
 {
 	int	i;
 	int	count;
+	int	start;
 
 	i = 0;
 	count = 0;
@@ -37,7 +38,31 @@ int	ft_count_args(const char *cmd)
 		ft_skip_whitespace(cmd, &i);
 		if (!cmd[i])
 			break ;
-		ft_skip_quotes(cmd, &i);
+		start = i;
+		if (cmd[i] == '\\')
+		{
+			i += 2;
+		}
+		else if (cmd[i] == '\'' || cmd[i] == '\"')
+		{
+			ft_skip_quotes(cmd, &i);
+		}
+		else if (cmd[i] == '>' || cmd[i] == '<')
+		{
+			i++;
+			if (cmd[i] == '>' || cmd[i] == '<')
+				i++;
+		}
+		else
+		{
+			while (cmd[i] && !ft_isspace(cmd[i]) && cmd[i] != '\'' && cmd[i] != '\"')
+			{
+				if (cmd[i] == '\\')
+					i += 2;
+				else
+					i++;
+			}
+		}
 		count++;
 	}
 	return (count);
