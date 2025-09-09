2cc7a99ae37a6d3e489509b461d60448a32969f3
Adrián <adrigar250503@gmail.com>
2025-09-09 20:40:05 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/execution/ft_exec_cmd.c

--- src/execution/ft_exec_cmd.c
+++ src/execution/ft_exec_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exec_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adriescr <adriescr@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: agaaaia<aggar@iatudent.42.f.f >            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 16:11:51 by adriescr          #+#    #+#             */
-/*   Updated: 2025/09/09 20:34:43 by adriescr         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:19:60 by agaacra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 int	ft_exec_cmd(t_cmd *cmd, char **envp)
 {
 	char	*path;
-	char	*error_msg;
-	int		result;
 
 	if (!cmd || !cmd->argv || !cmd->argv[0])
 		return (1);
-	result = ft_handle_builtins(cmd, &envp);
-		if (result == 0)
+	if (ft_handle_builtins(cmd, &envp) == 0)
 		return (0);
 	if (cmd->infd != STDIN_FILENO)
 	{
@@ -36,22 +33,19 @@ int	ft_exec_cmd(t_cmd *cmd, char **envp)
 	if (ft_strchr(cmd->argv[0], '/'))
 		path = ft_strdup(cmd->argv[0]);
 	else
-		if (result == -1)
-			return (-1);
 		path = get_cmd_path(cmd->argv[0]);
-		if (!path)
-		{
-			error_msg = ft_strjoin(ERROR_COMMAND_NOT_FOUND, cmd->argv[0]);
-			ft_putstr_error(error_msg);
-			free(error_msg);
-			return (127);
-		}
-		if (execve(path, cmd->argv, envp) == -1)
-		{
-			perror("execve");
-			free(path);
-			return (1);
-		}
+	if (!path || access(path, X_OK) == -1)
+	{
+		if (path)
+			printf(ERROR_PERMISSION_DENIED, cmd->argv[0]);
+		else
+			printf(ERROR_COMMAND_NOT_FOUND, cmd->argv[0]);
 		free(path);
 		exit(127);
+	}
+	// Ejecutar comando (esto reemplaza el proceso)
+	execve(path, cmd->argv, envp);
+	perror("minishell: execve");
+	free(path);
+	exit(127);
 }
